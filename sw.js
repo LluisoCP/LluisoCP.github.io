502cffa4b8cb1ed6bcee9dbd160012b35fe03e92
Lluis Camps <lluiso.aloha@gmail.com>
2022-05-23 14:06:37 +0200

    Manage PBS & notification click events in a single, common service worker. Manage PBS event by tag.

--- sw.js
+++ sw.js
@@ -4,6 +4,8 @@ const toCache         = [
   '/index.html',
   '/index.css'
 ]
+const swOptions = {includeUncontrolled: true, type:"window"}
+const playRegexp = /(\d+)@(\w+)/
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
@@ -24,4 +26,57 @@ self.addEventListener('activate', function(event) {
     caches.keys()
     .then(cacheNames => Promise.all(cacheNames.filter(cacheName => oldCaches.includes(cacheName)).map(cacheName => caches.delete(cacheName))))
   )
+})
+
+self.addEventListener("periodicsync", e => {
+  const tag = e.tag
+  pbsMatches = tag.match(playRegexp)
+  if (pbsMatches) {
+    const pbsMatch = pbsMatches[0]
+    if (~pbsMatch.indexOf("first")) {
+      self.registration.showNotification("First", {
+        body: pbsMatches[0],
+        icon: 'https://lluisocp.github.io/s/senzu_logo_medium.png',
+        // image: response.image ? response.image + '?size=75x75' : toCache[1],
+        image: 'https://lluisocp.github.io/img/logo_180.png',
+        vibrate: [300, 100, 300],
+        tag: "test_notif",
+        // badge: '/public/static/dist/img/senzu_black_logo.svg'
+        badge: 'https://lluisocp.github.io/s/second/senzu_black_logo_small.png'
+      })
+    } else if (~pbsMatch.indexOf("second")) {
+      self.registration.showNotification("Second", {
+        body: pbsMatches[0],
+        icon: 'https://lluisocp.github.io/s/senzu_logo_medium.png',
+        // image: response.image ? response.image + '?size=75x75' : toCache[1],
+        image: 'https://lluisocp.github.io/img/logo_180.png',
+        vibrate: [300, 100, 300],
+        tag: "test_notif",
+        // badge: '/public/static/dist/img/senzu_black_logo.svg'
+        badge: 'https://lluisocp.github.io/s/second/senzu_black_logo_small.png'
+      })
+    }
+  }
+})
+self.addEventListener("notificationclick", event => {
+  if (event.notification.tag === "test_notif") {
+    event.notification.close()
+    event.waitUntil(
+      clients.matchAll(swOptions)
+      .then(allClients => {
+        let currentClient;
+        for (let client of allClients) {
+          if(~client['url'].indexOf("lluisocp.github.io/s/second")) {
+            client.focus()
+            currentClient = client
+            break
+          }
+        }
+        if (!currentClient) {
+          return clients.openWindow("https://lluisocp.github.io/s/second/index.html")
+        }
+      })
+      .catch(e => console.log(e.name, e.message, e.lineNumber))
+    )
+  }
 })
